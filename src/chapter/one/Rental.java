be2ef5aa5de51cd5811fc5e23ac90cab2bf2c809
David Gómez <ldavid.gomez@gmail.com>
2022-06-05 17:30:41 +0200

    Change get charge to Movie

--- src/chapter/one/Rental.java
+++ src/chapter/one/Rental.java
@@ -14,30 +14,14 @@ public class Rental {
         return _movie;
     }
 
-    public double getCharge() {
-        double amount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (this.getDaysRented() > 2)
-                    amount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (this.getDaysRented() > 3)
-                    amount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
-    }
-
     public int getFrequentRenterPoints() {
         if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1)
             return 2;
 
         return 1;
+    }
+
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
     }
 }
