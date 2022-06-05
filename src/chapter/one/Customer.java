736758974b990e8a02c6be96121f4d601d480424
David Gómez <ldavid.gomez@gmail.com>
2022-06-05 12:17:47 +0200

    Remove variable

--- src/chapter/one/Customer.java
+++ src/chapter/one/Customer.java
@@ -27,10 +27,8 @@ class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "chapter.one.Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -39,8 +37,8 @@ class Customer {
                     each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) +
