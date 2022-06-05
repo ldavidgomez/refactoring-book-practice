f5dbbfa90ff8fb3b4cb8c80bc1e17ab690458fcb
David Gómez <ldavid.gomez@gmail.com>
2022-06-05 12:45:26 +0200

    Extract frequent renter point to an external method in Rental class

--- src/chapter/one/Customer.java
+++ src/chapter/one/Customer.java
@@ -25,16 +25,11 @@ class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
-        String result = "chapter.one.Rental Record for " + getName() + "\n";
+        String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+            frequentRenterPoints = each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
