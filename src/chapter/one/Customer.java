0b1ca5e36c5908a525a011c47be6ff37a7c3ac4e
David Gómez <ldavid.gomez@gmail.com>
2022-06-05 17:23:46 +0200

    Remove unnecessary code

--- src/chapter/one/Customer.java
+++ src/chapter/one/Customer.java
@@ -22,18 +22,13 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            //determine amounts for each line
-            frequentRenterPoints = each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) +
