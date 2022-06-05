afd5c151a60203ac51172ba45c7cd3b081e3c7d9
David Gómez <ldavid.gomez@gmail.com>
2022-06-05 17:46:59 +0200

    Move get charge

--- src/chapter/one/Price.java
+++ src/chapter/one/Price.java
@@ -3,38 +3,40 @@ package chapter.one;
 public  abstract class Price {
     abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        double amount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return amount;
-    }
+    abstract double getCharge(int daysRented);
 }
 class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    public double getCharge(int daysRented){
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
+
 }
 class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    public double getCharge(int daysRented){
+        return daysRented * 3;
     }
 }
 class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    public double getCharge(int daysRented){
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
+
 }
